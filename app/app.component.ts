5cd5c8df38104815eb72dad19047cbb842148450
Rizan Zaky <rizan149@gmail.com>
2016-10-29 10:23:29 +0530

    updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+			<h1>{{title}}</h1>
+    		<h2>My favorite hero is: {{myHero}}</h2>
+			<ul>
+				<li *ngFor = "let item of heroes">{{item}}</li>
+			</ul>
+			`
 })
-export class AppComponent { }
+export class AppComponent {
+	title: string;
+	myHero: string;
+	heroes: string[];
+
+	public constructor() {
+		this.title = 'Tour of Heroes';
+		this.myHero = 'Windstorm';
+		this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+    }
+}
